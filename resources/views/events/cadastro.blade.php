33191d20d12713c260898132ca6bece2b55b0665
FDMIRS <flaviorodriguescr@gmail.com>
2024-10-08 21:30:11 -0300

    first commit

---
+++ resources/views/events/cadastro.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section('title', 'Efetuar cadastro no sistema')
+ 
+@section('content')
+
+<h1>Preencha os campos abaixo para efetuar o seu cadastro</h1>
+
+@endsection
---
+++ resources/views/events/login.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section('title', 'Efetuar Login no sistema')
+ 
+@section('content')
+
+<h1>Insira nome de usuário e senha</h1>
+
+@endsection
